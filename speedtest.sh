6b16f02ffdc7be68034a155164551dc244c59b8e
chris-devel0per <101336067+chris-devel0per@users.noreply.github.com>
2022-08-08 20:26:37 +0300

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -2,23 +2,24 @@
 echo 'Running'
 variable=$(speedtest-cli)
 
-download=$(echo "$variable" | grep Download)
+download=$(echo "$variable" | grep "ERROR: <urlopen error [Errno -3] Temporary failure in name resolution>")
 
 number_download=$(echo "$(echo "$download" | tr -dc '0-9')" | rev | cut -c3- | rev)
-#download_number= $(echo "$download" | tr -dc '0-9')
 
-#download_number_cut=$(echo "$download_number" | rev | cut -c3- | rev)
-
-#echo "$download_number_cut"
+error =$(echo "$variable" | grep Download)
 
 echo "$number_download"
 
-if [ $number_download  -gt 80 ]
+if [ $number_download  -gt 70 ]
 then
        echo "--------------------------------------" >> high.txt
         echo "$variable" | grep Download >> high.txt
         echo "$variable" | grep Upload >> high.txt
         echo $(date) >> high.txt
+elif[ $variable ]; then 
+        echo "---------------------------------------" >> log.txt
+        echo "INTERNET CONNECTION LOST !!"  >> log.txt
+        echo $(date) >> log.txt
 else
         echo "---------------------------------------" >> log.txt
         echo "$variable" | grep Download >> log.txt
