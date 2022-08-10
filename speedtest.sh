3644796cc20be04bbaeb5f69723d783bcbaf102a
chris-devel0per <101336067+chris-devel0per@users.noreply.github.com>
2022-08-10 10:30:49 +0300

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -6,8 +6,6 @@ download=$(echo "$variable" | grep "ERROR: <urlopen error [Errno -3] Temporary f
 
 number_download=$(echo "$(echo "$download" | tr -dc '0-9')" | rev | cut -c3- | rev)
 
-#error =$(echo "$variable" | grep Download)
-
 echo "$number_download"
 
 if [ $number_download  -gt 70 ]
@@ -16,10 +14,6 @@ then
         echo "$variable" | grep Download >> high.txt
         echo "$variable" | grep Upload >> high.txt
         echo $(date) >> high.txt
-#elif[ $variable ]; then 
-#        echo "---------------------------------------" >> log.txt
-#        echo "INTERNET CONNECTION LOST !!"  >> log.txt
-#        echo $(date) >> log.txt
 else
         echo "---------------------------------------" >> log.txt
         echo "$variable" | grep Download >> log.txt
